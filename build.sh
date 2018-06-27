e6dabdc9d8dded49217ab6d9899ea3f63ce8ed0a
Leibniz.Hu <leibnizhu@gmail.com>
2018-06-27 15:24:54 +0800

    优化脚本和日志配置

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 name="docbuilder"
 port="8233"
 mvn clean package
+echo "Building new docbuilder docker image..."
 docker build -t docbuilder:v0.1 .
+echo "Stopping old docbuilder docker container..."
 docker stop ${name}
+echo "Deleting old docbuilder docker container..."
 docker rm ${name}
-docker run -i -t --name ${name} -p ${port}:8233 docbuilder:v0.1
+echo "Starting new docbuilder docker container..."
+docker run -d --restart always --name ${name} -p ${port}:8233 docbuilder:v0.1
